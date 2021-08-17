1417c62a1de7c61e92ed89b8a3161fb3cd45fc27
Brian Smith <brian.bubbam@gmail.com>
2021-08-17 13:58:01 -0500

    commit tree 1f41b66cbc24139c6d8b7283ff6ee5842c4b7b28

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+    
+    # returns an array of all items with user info
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    
+
+    # returns a user with an array of all items associated with that user
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, include: :items
+        #the include option will let us nest associated data in our response
+    end
 end
